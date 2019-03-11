8a2ca0ac685219b2178dd453a83e811369ce867e
Snigdha Regmi <sregmi@NORD.LOCAL>
2019-03-10 21:54:18 -0600

    styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -11,13 +11,14 @@ const Character = props => {
             </div> 
             <div>
             <p><strong>Birth Year: </strong> {props.character.birth_year}</p>
+            <p><strong>Created: </strong> {props.character.created}</p>
+            <p><strong>Eye Color:</strong> {props.character.eye_color}</p>
+            <p><strong>Films:</strong> {props.character.films.length}</p>
+            <p><strong>Gender:</strong> {props.character.gender}</p>
+            <p><strong>Hair Color:</strong> {props.character.hair_color}</p>
             <p><strong>Height: </strong> {props.character.height}</p>
             <p><strong>Mass: </strong> {props.character.mass}</p>
-            <p><strong>Hair Color:</strong> {props.character.hair_color}</p>
             <p><strong>Skin Color:</strong> {props.character.skin_color}</p>
-            <p><strong>Eye Color:</strong> {props.character.eye_color}</p>
-            <p><strong>Gender:</strong> {props.character.gender}</p>
-            <p><strong>Films:</strong> {props.character.films.length}</p>
         </div>
         </div>
     );
